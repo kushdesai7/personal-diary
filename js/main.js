554c177a0eeed49f83f5952e3691de3382a92b61
Dev-AkshDesai <dev.akshdesai@gmail.com>
2021-08-30 19:55:52 +0530

    QOL - Tabs over spaces

--- js/main.js
+++ js/main.js
@@ -3,19 +3,20 @@ const signInBtn = document.getElementById('SignIn')
 const container = document.getElementById('container')
 
 signInBtn.addEventListener('click', () => {
-    container.classList.remove('right-panel-active')
+	container.classList.remove('right-panel-active')
 });
 
 signUpBtn.addEventListener('click', () => {
-    container.classList.add('right-panel-active')
+	container.classList.add('right-panel-active')
 });
 
 const loader = document.getElementById('load-id')
 
 window.addEventListener('load', () => {
-    loader.style.display = 'none'
+	loader.style.display = 'none'
 })
 
 // SignInButton.addEventListener('click', () => {
-//     container.classList.add('right-panel-active')
+//	 container.classList.add('right-panel-active')
 // });
+
